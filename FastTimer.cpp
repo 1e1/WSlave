3f2ec630c85b483ba93423487a31f5f85edca06b
skan <peeplone@sf.net>
2012-09-06 17:12:12 +0200

    optimization

--- FastTimer.cpp
+++ FastTimer.cpp
@@ -9,8 +9,8 @@ boolean FastTimer::update()
 {
   _lastEmbedTime  = _embedTime;
   _embedTime      = EMBEDTIME;
-  //return (0x01 & _lastEmbedTime) != (0x01 & _embedTime);
-  return 0x01 & (_lastEmbedTime ^ _embedTime);
+  //return 0x01 & (_lastEmbedTime ^ _embedTime);
+  return _lastEmbedTime != _embedTime;
 }
 
 boolean FastTimer::isNewCycle()
