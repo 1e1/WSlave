af5f54e0526fceb96797d37c260cae593c071d5e
skan <peeplone@sf.net>
2012-09-07 17:33:09 +0200

    design - LCD draft

---
+++ LSlave.h
@@ -0,0 +1,20 @@
+/** 
+  * 
+  *   01234567890ABCDEF
+  *  +-----------------+
+  * 0|<NAME__10>  ##/PP|0
+  * 1|<VALUE________16>|1
+  *  +-----------------+
+  *   01234567890ABCDEF
+  * 
+  * UP/DOWN: page select
+  * LEFT/RIGHT: change value
+  * SELECT: ?
+  * 
+  */
+
+#ifndef LSLAVE_H_
+#define LSLAVE_H_
+
+
+#endif
