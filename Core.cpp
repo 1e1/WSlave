95317121eff14ab7ab2e61f8da050c817faa4793
skan <peeplone@sf.net>
2012-09-10 17:49:07 +0200

    read more than one body parameter

--- Core.cpp
+++ Core.cpp
@@ -19,12 +19,15 @@ namespace Core {
   
   void readLine(Stream *currentStream)
   {
-    uint8_t pin, value;
+    uint8_t pin, value, watchdog = digitals_len + pulses_len;
     _currentStream = currentStream;
-    _readUint8(pin);
-    _readUint8(value);
-    setDigitalAtPin(pin, value) || setPulseAtPin(pin, value);
-    LOG("SET pin #"); LOG(pin); LOG(" <- "); LOG(value); LOGLN(';');
+    // [0-9]+ OTHER [0-9]+ (OTHER [0-9]+ OTHER [0-9]+)
+    while (_currentStream.available() && watchdog--) {
+      _readUint8(pin);
+      _readUint8(value);
+      setDigitalAtPin(pin, value) || setPulseAtPin(pin, value);
+      LOG("SET pin #"); LOG(pin); LOG(" <- "); LOG(value); LOGLN(';');
+    }
   }
   
   /** 
