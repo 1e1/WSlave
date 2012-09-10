554018512b89520bd1ebff38609a5f8bceb2b777
skan <peeplone@sf.net>
2012-09-10 19:13:12 +0200

    skelton USB Slave

---
+++ USlave.h
@@ -0,0 +1,15 @@
+/** 
+  * READ: <pinNumber> '=' <value> ('&' <pinNumber> '=' <value>)*
+  * 
+  */
+
+#ifndef USLAVE_H_
+#define USLAVE_H_
+
+#include <Arduino.h>
+#include "Core.h"
+#include "config.h"
+#include "macros.h"
+
+
+#endif
