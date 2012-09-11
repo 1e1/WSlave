3058e267be4578650dfb4c5ed25042149caf6b2d
skan <peeplone@sf.net>
2012-09-11 15:00:45 +0200

    code indent

--- USlave.cpp
+++ USlave.cpp
@@ -4,28 +4,28 @@
 
 namespace USlave {
   
-void begin()
-{
-  Serial.begin(USB_SPEED);
-}
-
-
-void check()
-{
-  if (Serial.available()) {
-    LOGLN(">>> USB");
-    Core::readLine(&Serial);
-    _sendStatus();
-    _close:
-    // close the connection:
-    Serial.flush();
-    LOGLN("<<< USB");
+  void begin()
+  {
+    Serial.begin(USB_SPEED);
   }
-}
-
-void _sendStatus()
-{
   
-}
+  
+  void check()
+  {
+    if (Serial.available()) {
+      LOGLN(">>> USB");
+      Core::readLine(&Serial);
+      _sendStatus();
+      _close:
+      // close the connection:
+      Serial.flush();
+      LOGLN("<<< USB");
+    }
+  }
+  
+  void _sendStatus()
+  {
+    
+  }
 
 }
