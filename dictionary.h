4c9c5b35f6c97e683c0bc76c240808cbab20dfe4
Nýd <peeplone@sf.net>
2012-09-18 09:14:06 +0200

    group PSTR strings in Dictionary

---
+++ dictionary.h
@@ -0,0 +1,14 @@
+#ifndef DICTIONARY_H_
+#define DICTIONARY_H_
+
+
+namespace Dictionary {
+  LONGSTRING(relay11) = "relay1.1";
+  LONGSTRING(relay12) = "relay1.2";
+  LONGSTRING(relay13) = "relay1.3";
+  LONGSTRING(relay14) = "relay1.4";
+  LONGSTRING(led) = "pulse";
+}
+
+
+#endif DICTIONARY_H_
