1e8f48b83ccbe62b92b10eb77efd81681aade774
skan <peeplone@sf.net>
2012-09-18 17:40:33 +0200

    stuff more words

--- dictionary.h
+++ dictionary.h
@@ -3,11 +3,27 @@
 
 
 namespace Dictionary {
+    
   LONGSTRING(relay11) = "relay1.1";
   LONGSTRING(relay12) = "relay1.2";
   LONGSTRING(relay13) = "relay1.3";
   LONGSTRING(relay14) = "relay1.4";
+  LONGSTRING(relay15) = "relay1.5";
+  LONGSTRING(relay16) = "relay1.6";
+  LONGSTRING(relay17) = "relay1.7";
+  LONGSTRING(relay18) = "relay1.8";
+  
+  LONGSTRING(relay21) = "relay2.1";
+  LONGSTRING(relay22) = "relay2.2";
+  LONGSTRING(relay23) = "relay2.3";
+  LONGSTRING(relay24) = "relay2.4";
+  LONGSTRING(relay25) = "relay2.5";
+  LONGSTRING(relay26) = "relay2.6";
+  LONGSTRING(relay27) = "relay2.7";
+  LONGSTRING(relay28) = "relay2.8";
+  
   LONGSTRING(led) = "pulse";
+  
 }
 
 
