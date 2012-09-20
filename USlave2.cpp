07550aaebc99cbb6d65ee6f862bb01b7d4e4e53d
skan <peeplone@sf.net>
2012-09-20 17:40:04 +0200

    checkstyle

--- USlave2.cpp
+++ USlave2.cpp
@@ -22,19 +22,19 @@ void USlave2::check()
     // digitals
     Core2::copyToBuffer_P(PSTR("\n=== DIGITALS ===\n"));
     for (uint8_t i=0; i < Core2::digitals_len; i++) {
-      statusLine(Core2::digitals[i], (uint8_t) Core2::digitals[i].getValue());
+      USlave2::statusLine(Core2::digitals[i], (uint8_t) Core2::digitals[i].getValue());
     }
     // pulses
     Core2::copyToBuffer_P(PSTR("\n=== PULSES ===\n"));
     LOG("track2: ");LOGLN(Core2::pulses[0].getValue());
     for (uint8_t i=0; i < Core2::pulses_len; i++) {
-      statusLine(Core2::pulses[i], Core2::pulses[i].getValue());
+      USlave2::statusLine(Core2::pulses[i], Core2::pulses[i].getValue());
     }
     /*
     // messages
     Core2::copyToBuffer_P(PSTR("\n=== MESSAGES ===\n"));
     for (uint8_t i=0; i < Core2::messages_len; i++) {
-      statusLine(Core2::messages[i], Core2::messages[i].getValue());
+      USlave2::statusLine(Core2::messages[i], Core2::messages[i].getValue());
     }
     */
     Core2::copyToBuffer('\n');
@@ -58,7 +58,7 @@ void USlave2::statusLine(Connector connector, uint8_t value)
 {
   Core2::copyToBuffer(connector.getPin());
   Core2::copyToBuffer('\t');
-  Core2::copyToBuffer((uint8_t)value);
+  Core2::copyToBuffer(uint8_t(value));
   Core2::copyToBuffer('\t');
   Core2::copyToBuffer_P(connector.getLabel());
   Core2::copyToBuffer('\n');
