eafecf4c9e629cad6b852ea3c2689731037c4823
skan <peeplone@sf.net>
2012-10-02 16:11:46 +0200

    check style

--- FastTimer2.h
+++ FastTimer2.h
@@ -10,21 +10,24 @@
 
 
 //==>  >> 0: 1 unit of embedTime is 0.001s
-//-->  const unsigned long maxTime = 4294967295; // = 49d 17h 02m 47s
-//-->  const unsigned int maxTime = 65535; // = 65s
+//-->  const unsigned long maxTime        = 4294967295; // = 49d 17h 02m 47s
+//-->  const unsigned int maxTime         = 65535;      // = 65s
+//==>  >> 10: 1 unit of embedTime is 1.024s
+//-->  const uint8_t maxEmbedTime         = 255;        // = 4mn 21s 120ms
 //==>  >> 12: 1 unit of embedTime is 4.096s
-//-->  const unsigned int maxEmbedTime = 65535; // = 3d 02h 33mn 51s 360ms
-//     const unsigned int moduloEmbedTime = 63281; // = 3d 00h 00mn 00s 000ms
-//-->  const uint8_t maxEmbedTime = 255; // = 17mn 24s 480ms
-//     const uint8_t moduloEmbedTime = 219; // = 15mn 00s 000ms
+//-->  const unsigned int maxEmbedTime    = 65535;      // = 3d 02h 33mn 51s 360ms
+//     const unsigned int moduloEmbedTime = 63281;      // = 3d 00h 00mn 00s 000ms
+//-->  const uint8_t maxEmbedTime         = 255;        // = 17mn 24s 480ms
+//     const uint8_t moduloEmbedTime      = 219;        // = 15mn 00s 000ms
 //==>  >> 14: 1 unit of embedTime is 16.384s
-//-->  const uint8_t maxEmbedTime = 255; // = 1h 09mn 37s 920ms
-//     const uint8_t moduloEmbedTime = 219; // = 1h 00mn 00s 000ms
+//-->  const uint8_t maxEmbedTime         = 255;        // = 1h 09mn 37s 920ms
+//     const uint8_t moduloEmbedTime      = 219;        // = 1h 00mn 00s 000ms
 //==>  >> 16: 1 unit of embedTime is 65.536s
-//-->  const uint8_t maxEmbedTime = 255; // = 4h 38mn 31s 680ms
-//     const uint8_t moduloEmbedTime = 219; // = 4h 00mn 00s 000ms
+//-->  const uint8_t maxEmbedTime         = 255;        // = 4h 38mn 31s 680ms
+//     const uint8_t moduloEmbedTime      = 219;        // = 4h 00mn 00s 000ms
 
 /** located in macros.h, config.h define EMBEDTIME as:
+#define EMBEDTIME_1s_4m    ((uint8_t) (millis() >> 10))
 #define EMBEDTIME_4s_15m   ((uint8_t) (millis() >> 12))
 #define EMBEDTIME_4s_3d    ((unsigned int) (millis() >> 12))
 #define EMBEDTIME_16s_1h   ((uint8_t) (millis() >> 14))
