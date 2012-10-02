03c9695c837d74c465c806286b1ffebb7f980f73
skan <peeplone@sf.net>
2012-10-02 16:10:57 +0200

    check style

--- Core2.h
+++ Core2.h
@@ -12,19 +12,19 @@
 #include "Schedule.h"
 
 
-#define MONDAY    0 // MONTAG
-#define TUESDAY   1 // DIENSTAG
-#define WEDNESDAY 2 // MITTWOCH
-#define THURSDAY  3 // DONNERSTAG
-#define FRIDAY    4 // FREITAG
-#define SATURDAY  5 // SAMSTAG
-#define SUNDAY    6 // SONNTAG
+#define MONDAY    byte(0) // MONTAG
+#define TUESDAY   byte(1) // DIENSTAG
+#define WEDNESDAY byte(2) // MITTWOCH
+#define THURSDAY  byte(3) // DONNERSTAG
+#define FRIDAY    byte(4) // FREITAG
+#define SATURDAY  byte(5) // SAMSTAG
+#define SUNDAY    byte(6) // SONNTAG
 
-#define READBUFFERSIZE          max(16, 1+ ARRAYLEN(HTTP_AUTH64))
-#define WRITEBUFFERSIZE         64
-#define MAXLINESIZE             255
+#define READBUFFERSIZE          max(byte(16), byte(1+ ARRAYLEN(HTTP_AUTH64)))
+#define WRITEBUFFERSIZE         byte(64)
+#define MAXLINESIZE             byte(255)
 
-#define READCHAR_TIMEOUT        4
+#define READCHAR_TIMEOUT        byte(4)
 
 
 /** ===================== **/
