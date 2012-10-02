0c07ab764686d6f66b4a72f520b32c3d63f7c3d8
skan <peeplone@sf.net>
2012-10-02 17:05:29 +0200

    change time unit

--- config.h
+++ config.h
@@ -60,7 +60,8 @@
 
 // VELOCITY (choose inside macros.h)
 //#define EMBEDTIME EMBEDTIME_65s_4h
-#define EMBEDTIME EMBEDTIME_16s_1h
+//#define EMBEDTIME EMBEDTIME_16s_1h
+#define EMBEDTIME EMBEDTIME_1s_4m
 
 /*
 // design conf
