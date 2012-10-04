cc106bd516b9de05a06ccd08f5f35b3ca226ff23
skan <peeplone@sf.net>
2012-10-04 18:52:55 +0200

    use structure instead

--- Schedule.cpp
+++ Schedule.cpp
@@ -3,13 +3,30 @@
 #include <Arduino.h>
 
 
-
-Schedule::Schedule(byte id, const prog_char* label, const boolean isNC, unsigned int data, Array<const uint8_t>* digitals)
+/*
+Schedule::Schedule(byte id, const prog_char* label, const boolean isNC, const unsigned int data, const uint8_t* digitals)
 {
   this->_pin      = (id << 2) | isNC;
   this->_label    = label;
   this->_schedule = data;
   this->_digitals = digitals;
+}
+*/
+
+Schedule::Schedule(byte id, const prog_char* label, const boolean isNC, const unsigned int data, const unsigned long digitals_32_49)
+{
+  this->init(id, label);
+  this->_id.isNC  = isNC;
+  this->_schedule = data;
+  this->_pins     = digitals_32_49;
+}
+
+Schedule::Schedule(byte id, const prog_char* label, const boolean isNC, const schedule data, const unsigned long digitals_32_49)
+{
+  this->init(id, label);
+  this->_id.isNC  = isNC;
+  this->_schedule2= data;
+  this->_pins     = digitals_32_49;
 }
 
 
